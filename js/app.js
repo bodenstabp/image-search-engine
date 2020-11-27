f04012b57a5db71a370535078edb22957260fb9c
Paul Bodenstab <bodenstabp@gmail.com>
2020-11-27 11:04:48 -0500

    Beautification

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ baguetteBox.run('.grid-layout');
 function searchItems () {
     const input = document.getElementById('searchBar');
     const toLower = input.value.toLowerCase();
-    const images = document.getElementsByTagName('img');
+    const images = document.getElementsByClassName('img-container');
     
     for ( let i = 0; i < images.length; i++ ) {
         const captions = document.getElementsByTagName('a')[i].getAttribute('data-caption');
